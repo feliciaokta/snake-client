edb97ca99a908fe7bf5a0781ba93bec2e22521e8
felicia puspita yuwono <feliciaokta@outlook.com>
2021-05-05 14:13:45 -0700

    exported const connect to play.js

---
+++ client.js
@@ -0,0 +1,39 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "135.23.222.131",
+    port: 50542,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("Connection is made with the server");
+  });
+
+  conn.on("data", (data) => {
+    // code that does something when the connection is first established
+    console.log("Server says: ", data);
+  });
+  
+  return conn;
+};
+
+// conn.on("event name", () => {      -- this "event name" is built-in 
+//   // code that does something
+// });
+
+/* options for "event name":
+- close         - error
+- connect       - lookup
+- data          - ready
+- drain         - timeout
+- end
+https://nodejs.org/api/net.html#net_class_net_socket
+*/
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,36 +1,11 @@
 // IP address: 135.23.222.131
 // PORT: 50542
 
-const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("Connection is made with the server");
-  });
-
-  conn.on("data", (data) => {
-    // code that does something when the connection is first established
-    console.log("Server says: ", data);
-  });
-  
-  return conn;
-};
-
-// conn.on("event name", () => {
-//   // code that does something
-// });
-
+// const net = require("net");
 
 
 console.log("Connecting ...");
 connect();
+
